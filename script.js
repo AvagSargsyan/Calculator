75a7680f849472d119021aa1cb1147bec1e254f2
Avag <avagsargsyan01@gmail.com>
2022-06-06 16:35:58 +0400

    Add basic functionality for simple operations

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const display = document.querySelector('.display');
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equals');
+const acButton = document.querySelector('.ac');
+
+let num1,
+    num2,
+    operation;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -38,3 +48,53 @@ function operate(operator, num1, num2) {
     }
     return 'No such operation!';
 }
+
+function displayOutput(str) {
+    display.textContent += str;
+}
+
+function ac() {
+    display.textContent = '';
+    num1 = undefined;
+    num2 = undefined;
+    operation = undefined;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        displayOutput(button.textContent);
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (operation) {
+            num2 = +(display.textContent.split(operation)[1]);
+            display.textContent = '';
+            displayOutput(operate(operation, num1, num2));
+
+            //should take the result as num1 and do the new operation on num1 and the number that will be pushed next.
+            
+        } else {
+            num1 = +display.textContent;
+            operation = button.textContent;
+            displayOutput(button.textContent);
+            console.log(num1, operation);
+        }
+    });
+});
+
+equalsButton.addEventListener('click', (e) => {
+    if (operation) {
+        num2 = +(display.textContent.split(operation)[1]);
+
+        display.textContent = '';
+        displayOutput(operate(operation, num1, num2));
+        console.log(display.textContent);
+        console.log(num2);
+    }
+});
+
+acButton.addEventListener('click', (e) => {
+    ac();
+})
